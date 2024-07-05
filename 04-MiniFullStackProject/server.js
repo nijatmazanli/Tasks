6a9d25ba03665cd76d4755aa19103dc01182e54b
poseydonianexe <prime@prime>
2024-07-05 15:49:26 +0400

    04-MiniFullStackProject update

--- 04-MiniFullStackProject/server.js
+++ 04-MiniFullStackProject/server.js
@@ -1,73 +1,123 @@
 const express = require("express")
+const fs = require('fs');
+const ejs = require("ejs")
+
+let data1 = fs.readFileSync('products.json');
+let products = JSON.parse(data1);
+
+
 const app = express()
-const port = 4000
-const fs = require("fs")
+app.set("view engine", "ejs")
+app.use(express.json())
+app.use(express.urlencoded())
 
-let product_data = fs.readFileSync("./products.json")
-let products = JSON.parse(product_data)
-console.log(products)
+// PRODUCTS ROUTES
+app.get("/products", (req, res)=>{
+    const data = products
+    if(data){    
+        res.render("products", {products})
 
-let user_data = fs.readFileSync("./users.json")
-let users = JSON.parse(user_data)
-console.log(users)
-//<------------ Products ---------->
 
-app.get("/products", (req,res) =>{
-    res.send("products")
+    }else{
+        res.status(404).json({
+            "status": 404,
+            "error": "Products not found!"
+        })
+    }
+
 })
 
-app.get("/product/:id", (req,res)=> {
-    res.send("product ;ist")
-    res.send(`Product name ${products}`)
+app.post("/product/new", (req, res)=>{
+    const data = req.body
+    const id = new Date()
+    const new_product = {
+        "id": id.getTime(),
+        "name": data.name,
+        "desc": data.desc,
+        "price": data.price,
+        "category": data.category,
+        "stock": data.stock,
+        "imageUrl": data.imageUrl,
+        "createdAt": new Date(),
+        "updatedAt": new Date()
+    }
+
+    products.products.push(new_product); 
+
+    let updatedData = JSON.stringify(products, null, 2);
+    // let products = updatedData
+    fs.writeFileSync('products.json', updatedData);
+    res.render("products", {products})
 })
 
-app.delete("/product/:id", (req,res)=> {
-    res.send("delete product")
+app.get("/create", (req, res)=>{
+    res.render("index")
 })
 
-app.post("/product/new",(req,res)=>{
-    res.send("add product")
-})
+app.delete("/product/:id", (req, res)=>{
 
-app.put("/product/:id",(req,res)=>{
-    res.send("put specific product")
 })
 
 
-app.patch("/product/:id", (req,res)=> {
-    res.send("patch product")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.get("/product/:id", (req, res)=>{
+    res.send("spesific product")
 })
 
 
-//<------------ Users ---------->
-app.get("/users", (req,res) =>{
+
+
+
+app.put("/product/:id", (req, res)=>{
+    res.send("put spesific product")
+})
+
+app.patch("/product/:id", (req, res)=>{
+    res.send("patch spesific product")
+})
+
+// USERS ROUTES
+app.get("/users", (req, res)=>{
     res.send("users")
 })
 
-
-app.get("/user/:id", (req,res)=> {
-    res.send("product ;ist")
+app.get("/user/:id", (req, res)=>{
+    res.send("spesific user")
 })
 
-app.delete("/user/:id", (req,res)=> {
-    res.send("delete product")
+app.delete("/user/:id", (req, res)=>{
+    res.send("delete spesific user")
 })
 
-app.post("/user/new",(req,res)=>{
-    res.send("add product")
+app.post("/user/new", (req, res)=>{
+    res.send("create user")
 })
 
-app.put("/user/:id",(req,res)=>{
-    res.send("put specific product")
+app.put("/user/:id", (req, res)=>{
+    res.send("put spesific user")
 })
 
-
-app.patch("/user/:id", (req,res)=> {
-    res.send("patch product")
+app.patch("/user/:id", (req, res)=>{
+    res.send("patch spesific user")
 })
 
+const PORT = 3000
 
-
-app.listen(port,() =>{
-    console.log("Fddfd");
+app.listen(PORT, ()=>{
+    // console.log(App is running on port: ${PORT})
 })
